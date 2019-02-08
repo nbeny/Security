033a4749f280f084d699822bd0b6f38b9fa93e7b
root <root@localhost.localdomain>
2019-02-08 17:31:51 +0100

    added spoofer and opti with interrupt key method

--- network_scanner.py
+++ network_scanner.py
@@ -1,24 +1,24 @@
 #!/usr/bin/env python
 
 import scapy.all as scapy
-import optparse
+import argparse
 
 
 def get_arguments():
-    parser = optparse.OptionParser()
-    parser.add_option("-t", "--target", dest="target", help="Target IP / IP range.")
-    (options, arguments) = parser.parse_args()
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-t", "--target", dest="target", help="Target IP / IP range.")
+    options = parser.parse_args()
     return options
 
 def scan(ip):
     arp_request = scapy.ARP(pdst=ip)
     broadcast = scapy.Ether(dst="ff:ff:ff:ff:ff:ff")
     arp_request_broadcast = broadcast/arp_request
-    answered_list = scapy.srp(arp_request_boardcast, timeout=1, verbose=False)[0]
+    answered_list = scapy.srp(arp_request_broadcast, timeout=1, verbose=False)[0]
     clients_list = []
     for element in answered_list:
         client_dict = {"ip": element[1].psrc, "mac": element[1].hwsrc}
-        client_list.append(client_dict)
+        clients_list.append(client_dict)
     return clients_list
 
 def print_result(results_list):
@@ -26,6 +26,10 @@ def print_result(results_list):
     for client in results_list:
         print(client["ip"] + "\t\t" + client["mac"])
 
-options = get_arguments()
-scan_result = scan(options.target)
-print_result(scan_result)
+
+try:
+    options = get_arguments()
+    scan_result = scan(options.target)
+    print_result(scan_result)
+except KeyboardInterrupt:
+    print("[-] Detected ctrl + C ... Quitting.")
